c776807682e50a03462835d2f5cb0563a6c7838d
Kamlesh Katolkar <kamlesh.alphains@gmail.com>
2020-04-01 15:52:48 +0200

    added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,10 +48,16 @@
                 policy_number : {
                     required: true,
                 },
+                premium : {
+                    required: true,
+                },
             },
             messages : {
                 policy_number: {
                     required: "Please enter policy number"
+                },
+                premium: {
+                    required: "Please enter premium amount"
                 },
             }
         });
